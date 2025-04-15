7f766d779ea0fa3b2bddc62ef3149c650b2980d1
Lemon <Eatun_C@163.com>
2025-04-15 16:44:24 +0800

    MVsB5_BT_Audio_SDK_V0.3.1 - *.cproject *.sag *.bat

--- BT_Audio_APP/tools/script.bat
+++ BT_Audio_APP/tools/script.bat
@@ -14,5 +14,7 @@ tools\remind_script\MergeAudio2BinNew.exe -a 0x0 -i ..\..\mp2
 rd /s /q mp2
 fc tools\remind_script\sound_remind_item.h app_framework\audio_engine\remind_sound_item.h
 if %errorlevel%==1 (copy tools\remind_script\sound_remind_item.h app_framework\audio_engine\remind_sound_item.h)
+grep '^#define CFG_AI_DENOISE_EN' app_src\system_config\app_config.h | grep '//'
+if %errorlevel%==1 (nds_ldsag.exe -t tools\nds32_template.txt tools\BP15x_AI.sag -o BP15x.ld) else (nds_ldsag.exe -t tools\nds32_template.txt tools\BP15x.sag -o BP15x.ld)
 
 
