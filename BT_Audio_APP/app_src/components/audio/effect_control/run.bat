3a4f3512ff20d9f15d26005cca6446981a79a646
Lemon <Eatun_C@163.com>
2025-04-16 10:53:18 +0800

    MVsB5_BT_Audio_SDK_V0.9.0 - *.pdf *.ini *.chm *.bat

--- BT_Audio_APP/app_src/components/audio/effect_control/run.bat
+++ BT_Audio_APP/app_src/components/audio/effect_control/run.bat
@@ -1,2 +1,2 @@
-.\v3_params_func_gen.exe -e params_msg_mapping.xlsx -b ..\..\..\..\middleware\roboeffect\roboeffect_lib\roboeffect_library_info_v2.25.4.bin -p ..\music_parameter -o ..\effect_control -w
+.\v3_params_func_gen.exe -e params_msg_mapping.xlsx -b ..\..\..\..\middleware\roboeffect\roboeffect_lib\roboeffect_library_info_v2.25.5.bin -p ..\music_parameter -o ..\effect_control -w
 pause
